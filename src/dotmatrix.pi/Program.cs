f91aad090bcdb8cce07cf4253971f7329817ab20
Daniel Gray <daniel.gray@puregym.com>
2025-02-25 19:23:24 +0000

    Remove cancel hook

--- src/dotmatrix.pi/Program.cs
+++ src/dotmatrix.pi/Program.cs
@@ -1,21 +1,12 @@
 ﻿using dotmatrix.lib.Matrix;
 
-using var matrix = new RGBLedMatrix(new RGBLedMatrixOptions { Rows = 32, Cols = 64 });
+using var matrix = new RGBLedMatrix(new RGBLedMatrixOptions { Rows = 64, Cols = 64, ChainLength = 4, HardwareMapping = "adafruit-hat", Parallel = 1 });
 var canvas = matrix.CreateOffscreenCanvas();
 
 var maxBrightness = matrix.Brightness;
 var rnd = new Random();
-
-// run until user presses Ctrl+C
-var running = true;
-Console.CancelKeyPress += (_, e) =>
-{
-    running = false;
-    e.Cancel = true; // do not terminate program with Ctrl+C, we need to dispose
-};
-
 var color = new Color(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
-while (running)
+while (true)
 {
     if (matrix.Brightness < 1)
     {
